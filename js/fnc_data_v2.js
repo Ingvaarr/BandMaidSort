c69f1e5432b16a66f3b7cebb83ea347b370c2b44
Ingvaarr <87226004+Ingvaarr@users.noreply.github.com>
2025-01-18 20:13:54 +0200

    new single
    Zen added

--- js/fnc_data_v2.js
+++ js/fnc_data_v2.js
@@ -183,6 +183,7 @@ var ary_CharacterData = [
   [1, "matchless GUM",                        [0,0,0,0,0,0,0,0,0,0,1], "BMA/YOLO.jpg"],
   [1, "Don't be Long",                        [0,0,0,0,0,0,0,0,0,0,1], "BMA/Different.jpg"],
   [1, "Hibana",                               [0,0,0,0,0,0,0,0,0,0,1], "BMA/Sense.jpg"],
+  [1, "Zen",                                  [0,0,0,0,0,0,0,0,0,0,1], "BMA/Zen.jpg"],
     
   [1, "Smile(Acoustic)",                      [0,0,0,0,0,0,0,0,0,0,0,1], "BMA/Acoustic.jpg"],
   [1, "At the drop of a hat(Acoustic)",       [0,0,0,0,0,0,0,0,0,0,0,1], "BMA/Acoustic.jpg"],
